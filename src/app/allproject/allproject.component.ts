e107972444f03f0cd921d0b4556f96942c79635d
samoranobaba <samoranobaba@gmail.com>
2025-03-05 12:20:02 +0100

    new fixes

--- src/app/allproject/allproject.component.ts
+++ src/app/allproject/allproject.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  ViewChild,
+  OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-allproject',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './allproject.component.html',
   styleUrl: './allproject.component.css'
 })
-export class AllprojectComponent {
-  constructor(private router:Router){}
+export class AllprojectComponent implements OnInit {
+  ngOnInit(){
+    if (isPlatformBrowser(this.platformId)) {
+    }
+    document.getElementById("home")?.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private router:Router){}
   onHome(){
     this.router.navigateByUrl('home');
   }
