625aea9a6d8f7fcb7d384ef361095606bc6cfe05
Ayokanmi Samuel <ayokanmi.samuel@abbaandking.com>
2025-04-09 17:16:16 +0100

    latest update

--- src/app/projbody/projbody.component.ts
+++ src/app/projbody/projbody.component.ts
@@ -1,36 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxMarqueeComponent } from '@omnedia/ngx-marquee';
-import 'intersection-observer';
-import { ContactComponent } from "../contact/contact.component";
-
-
+// import 'intersection-observer';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-projbody',
   standalone: true,
   imports: [NgxMarqueeComponent, ContactComponent],
   templateUrl: './projbody.component.html',
-  styleUrl: './projbody.component.css'
+  styleUrl: './projbody.component.css',
 })
 export class ProjbodyComponent {
-constructor(private router:Router){}
-onHome(){
-  this.router.navigateByUrl('home');
-}
-onConnect(){
-  this.router.navigateByUrl('about');
-}
-onExpertise(){
-  this.router.navigateByUrl('expertise');
-}
-onProject(){
-  this.router.navigateByUrl('project');
-}
-onResume(){
-  this.router.navigateByUrl('resume');
-}
-onContact(){
-  this.router.navigateByUrl('contact')
-}
+  constructor(private router: Router) {}
+  // const IntersectionObserver = await import('intersection-observer');
+
+  onHome() {
+    this.router.navigateByUrl('home');
+  }
+  onConnect() {
+    this.router.navigateByUrl('about');
+  }
+  onExpertise() {
+    this.router.navigateByUrl('expertise');
+  }
+  onProject() {
+    this.router.navigateByUrl('project');
+  }
+  onResume() {
+    this.router.navigateByUrl('resume');
+  }
+  onContact() {
+    this.router.navigateByUrl('contact');
+  }
 }
