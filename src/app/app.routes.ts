afad470c16105b509e2ea0e83278c440208abfe5
Ayokanmi Samuel <ayokanmi.samuel@abbaandking.com>
2025-04-09 17:42:43 +0100

    Narrow escape

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,1 @@
-import { Routes } from '@angular/router';
-import { FullprojectComponent } from './fullproject/fullproject.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ExpertComponent } from './expert/expert.component';
-import { ProjbodyComponent } from './projbody/projbody.component';
-import { ContactComponent } from './contact/contact.component';
-import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: FullprojectComponent },
-  { path: 'about', component: ProfileComponent },
-  { path: 'expertise', component: ExpertComponent },
-  { path: 'project', component: ProjbodyComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'resume', component: ResumeComponent },
-];
